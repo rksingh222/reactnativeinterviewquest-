2f6b3ad2afe52898ddd68c892d556f37b0c772b7
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-08-18 01:37:29 +0530

    Update vavodigital.js

--- vavodigital.js
+++ vavodigital.js
@@ -2,3 +2,10 @@ Interview Question from Vavo Digital (React Native)
 1. what are bridges
 2. difference between flexbox of reactnative and web app
 3. what is scrollView
+4. What is state management
+5. what is useEffect
+6. what is lifecycle of React Native
+7. what is alias of useEffect in the lifecycle of react native
+8. what are hooks
+9. what is flexbox
+10. difference between states and props
